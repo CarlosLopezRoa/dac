2af34c71401653ec9556f035751898451a3ead3e
Carlos <carlos.lopez.roa@gmail.com>
2019-04-11 17:06:48 +0200

    search for the column on test

--- Rec.py
+++ Rec.py
@@ -123,7 +123,7 @@ if preprocess:
     with open("predictorstrain.p","rb") as filehandler:
         predictors = pickle.load(filehandler)
     for col in tqdm(predictors.keys()):
-        if col not in [99]:
+        if col not in [39]:
             print(col)
             not_nan_col_lines = train.loc[train.loc[:,col].isna(), not_nan_cols_dict[col]].dropna()
             if col in list(range(14)):
